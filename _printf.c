d6190a74a0840c057c360b4e854248eab35f42f3
Slonjoh <tobson11122@gmail.com>
2023-08-19 14:23:45 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -1,30 +1,53 @@
 #include "main.h"
-
+#include <stdlib.h>
+#include <stdarg.h>
 /**
- * _printf - Produces output according to a format
- * @format: Is a character string. The format string
- * is composed of zero or more directives
- *
- * Return: The number of characters printed (excluding
- * the null byte used to end output to strings)
- **/
+ * _printf - print anything
+ * @format: arguments
+ * Return: number of characters printed
+ */
 int _printf(const char *format, ...)
 {
-	int size;
-	va_list args;
+	va_list arguments;
+	const char *p;
+	int num = 0;
 
 	if (format == NULL)
 		return (-1);
-
-	size = _strlen(format);
-	if (size <= 0)
-		return (0);
-
-	va_start(args, format);
-	size = handler(format, args);
-
-	_putchar(-1);
-	va_end(args);
-
-	return (size);
+	va_start(arguments, format);
+	for (p = format; *p; p++)
+	{
+		if (*p == '%' && *p + 1 == '%')
+		{
+			_putchar(*p), num++;
+			continue;
+		}
+		else if (*p == '%' && *p + 1 != '%')
+		{
+			switch (*++p)
+			{
+				case 's':
+					num += fun_string(arguments);
+					break;
+				case 'c':
+					num += fun_character(arguments);
+					break;
+				case '%':
+					_putchar('%'), num++;
+					break;
+				case '\0':
+					return (-1);
+				case 'i':
+				case 'd':
+					num += fun_integer(arguments);
+					break;
+				default:
+					_putchar('%'), _putchar(*p), num += 2;
+			}
+		}
+		else
+			_putchar(*p), num++;
+	}
+va_end(arguments);
+return (num);
 }
