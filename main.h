48b6b1370e4f1cd4ade19f66688bc01674f09ea6
Slonjoh <tobson11122@gmail.com>
2023-08-19 15:54:07 -0700

    putchar

--- main.h
+++ main.h
@@ -6,8 +6,27 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+ * struct buffer - A buffer structure for handling formatted output.
+ * @str: The buffer string.
+ * @size: The size of the buffer.
+ * @index: The current index within the buffer.
+ * @overflow: The overflow count for the buffer.
+ */
+typedef struct buffer {
+    char *str;
+    size_t size;
+    size_t index;
+    int overflow;
+} buffer;
 
-/* Function prototype */
+/* Function prototypes */
+buffer *buf_new(void);
+void buf_wr(buffer *buf);
+void buf_inc(buffer *buf);
+void buf_write(buffer *buf);
+void buf_end(buffer *buf);
 int _printf(const char *format, ...);
+int _putchar(char c);
 
 #endif /*MAIN_H*/
