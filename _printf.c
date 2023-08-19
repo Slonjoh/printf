f0f95868094faf0214fb082b2d369889c77c48ba
Slonjoh <tobson11122@gmail.com>
2023-08-19 10:44:26 -0700

    Printf

--- _printf.c
+++ _printf.c
@@ -11,71 +11,57 @@
 
 int _printf(const char *format, ...)
 {
-
 	va_list the_arguments;
-	int count = 0;
-	int a;
-	char s;
+        int count = 0;
+        int a;
+        char s;
 
-	buffer *buf = buf_new();
-	if (buf == NULL)
-		return (-1);
+        va_start(the_arguments, format);
 
-	va_start(the_arguments, format);
+        if (!format)
+                return (-1);
 
-	if (!format)
-	{
-		buf_end(buf);
-		va_end(the_arguments);
-		return (-1);
-	}
+        for (a = 0; format[a] != '\0'; a++)
+        {
+                if (format[a] != '%')
+                {
+                        write(1, &format[a], 1);
+                        count++;
+                }
+                else
+                {
+                        a++;
+                        if (format[a] == 'c')
+                        {
+                                s = va_arg(the_arguments, int);
+                                write(1, &s, 1);
+                                count++;
+                        }
+                        else if (format[a] == 's')
+                        {
+                                char *str = va_arg(the_arguments, char *);
+                                if (str)
+                                {
+                                        int len = 0;
+                                        while (str[len] != '\0')
+                                                len++;
+                                        write(1, str, len);
+                                        count += len;
+                                }
+                        }
+                        else if (format[a] == '%')
+                        {
+                                write(1, &format[a], 1);
+                                count++;
+                        }
+                        else
+                        {
+                                write(1, "%", 1);
+                                write(1, &format[a], 1);
+                                count += 2;
+                        }
+                }
+        }
 
-	for (a = 0; format[a] != '\0'; a++)
-	{
-		buf_wr(buf);
-		if (format[a] == '%')
-		{
-			a++;
-			if (format[a] == 'c')
-			{
-				s = va_arg(the_arguments, int);
-				buf->str[buf->index] = s;
-				buf_inc(buf);
-				count++;
-			}
-			else if (format[a] == 's')
-			{
-				char *str = va_arg(the_arguments, char *);
-				if (str)
-				{
-					int len = 0;
-					while (str[len] != '\0')
-						len++;
-					for (int i = 0; i < len; i++)
-					{
-						buf->str[buf->index] = str[i];
-						buf_inc(buf);
-					}
-					count += len;
-				}
-			}
-			else if (format[a] == '%')
-			{
-				buf->str[buf->index] = '%';
-				buf_inc(buf);
-				count++;
-			}
-		}
-		else
-		{
-			buf->str[buf->index] = format[a];
-			buf_inc(buf);
-			count++;
-		}
-	}
-	buf_write(buf);
-	buf_end(buf);
-
-	va_end(the_arguments);
-	return (count);
-}
+        va_end(the_arguments);
+        return (count);
