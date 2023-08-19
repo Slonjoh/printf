03c2a1ffb382d6c394bb4b4d51c031b46ed91903
Slonjoh <tobson11122@gmail.com>
2023-08-19 14:28:54 -0700

    Printf

--- _printf.c
+++ _printf.c
@@ -1,45 +1,68 @@
 #include "main.h"
-#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
 
 /**
- * _printf - prints any string with certain flags for modification
- * @format: the string of characters to write to buffer
- * Return: an integer that counts how many writes to the buffer were made
+ * _printf - Produces output according to a format.
+ * @format: The format string containing directives.
+ *
+ * Return: The number of characters printed (excluding the null byte).
  */
+
 int _printf(const char *format, ...)
 {
-	int i = 0, var = 0;
-	va_list v_ls;
-	buffer *buf;
+	va_list the_arguments;
+        int count = 0;
+        int a;
+        char s;
 
-	buf = buf_new();
-	if (buf == NULL)
-		return (-1);
-	if (format == NULL)
-		return (-1);
-	va_start(v_ls, format);
-	while (format[i])
-	{
-		buf_wr(buf);
-		if (format[i] == '%')
-		{
-			var = opid(buf, v_ls, format, i);
-			if (var < 0)
-			{
-				i = var;
-				break;
-			}
-			i += var;
-			continue;
-		}
-		buf->str[buf->index] = format[i];
-		buf_inc(buf);
-		i++;
-	}
-	buf_write(buf);
-	if (var >= 0)
-		i = buf->overflow;
-	buf_end(buf);
-	va_end(v_ls);
-	return (i);
+        va_start(the_arguments, format);
+
+        if (!format)
+                return (-1);
+
+        for (a = 0; format[a] != '\0'; a++)
+        {
+                if (format[a] != '%')
+                {
+                        write(1, &format[a], 1);
+                        count++;
+                }
+                else
+                {
+                        a++;
+                        if (format[a] == 'c')
+                        {
+                                s = va_arg(the_arguments, int);
+                                write(1, &s, 1);
+                                count++;
+                        }
+                        else if (format[a] == 's')
+                        {
+                                char *str = va_arg(the_arguments, char *);
+                                if (str)
+                                {
+                                        int len = 0;
+                                        while (str[len] != '\0')
+                                                len++;
+                                        write(1, str, len);
+                                        count += len;
+                                }
+                        }
+                        else if (format[a] == '%')
+                        {
+                                write(1, &format[a], 1);
+                                count++;
+                        }
+                        else
+                        {
+                                write(1, "%", 1);
+                                write(1, &format[a], 1);
+                                count += 2;
+                        }
+                }
+        }
+
+        va_end(the_arguments);
+        return (count);
 }
