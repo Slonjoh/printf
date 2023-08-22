a7ae47bae2e3f5c615188053d0086c2591096813
slonjoh <tobson11122@gmail.com>
2023-08-22 09:53:05 -0700

    formatted output

---
+++ put_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * put_string - a function that prints a string
+ * @str: a string
+ * Return: number of strings
+ */
+
+int put_string(char *str)
+{
+	int i, lens = 0;
+
+	if (str == NULL)
+	{
+		str = "(null)";
+		for (i = 0; i < 6; i++)
+			_putchar(str[i]);
+		return (6);
+	}
+	while (str[lens] != '\0')
+	{
+		_putchar(str[lens]);
+		lens++;
+	}
+	return (lens);
+}
